e3c39c381935eb58daafabaf0e34ac6152bd25bb
оло <olo@MacBook-Pro-olo.local>
2021-03-28 20:14:57 +0300

    fix : route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { Switch, Route, Redirect, BrowserRouter as Router} from "react-router-dom";
+import {
+  Switch,
+  Route,
+  Redirect,
+  BrowserRouter as Router,
+} from "react-router-dom";
 
+import { TextBook, Home, Units, Words, Settings } from "pages";
+import { SideBar } from "components/SideBar";
 import "./App.css";
-import SideBar from './components/SideBar';
-import { TextBook ,Home, Units, Words, Settings} from "./pages";
 
 const App: React.FC = () => {
   return (
@@ -13,18 +18,25 @@ const App: React.FC = () => {
           <SideBar />
         </div>
         <div className="body__content">
-            <Switch>
-              <Route exact component={Home} path="/" />
-              <Route exact component={Settings} path="/settings" />
-              <Route exact component={TextBook} path="/textbook" />
-              <Route exact component={Units} path="/textbook/category/:number?" />
-              <Route component={Words} path="/textbook/category/:number?/unit/:number?" />
-              <Redirect from="/" to="/" />
-            </Switch>
+          <Switch>
+            <Route exact component={Home} path="/" />
+            <Route exact component={Settings} path="/settings" />
+            <Route exact component={TextBook} path="/textbook" />
+            <Route
+              exact
+              component={Units}
+              path="/textbook/category/:categoryId?"
+            />
+            <Route
+              component={Words}
+              path="/textbook/category/:categoryId/:unitId"
+            />
+            <Redirect from="/" to="/" />
+          </Switch>
         </div>
       </Router>
     </div>
-    );
+  );
 };
 
 export default App;
